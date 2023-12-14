527e50853c4dc38c3b8f0695ff79ac904076f540
njtierney <nicholas.tierney@gmail.com>
2023-12-14 17:59:12 +1000

    add the functions to add pct_mortality to the data, includ emplogit and IHS transforms

---
+++ R/add_pct_mortality.R
@@ -0,0 +1,44 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param ir_data_raw
+#' @param no_dead
+#' @param no_tested
+#' @return
+#' @author njtierney
+#' @export
+add_pct_mortality <- function(ir_data_raw, no_dead, no_tested) {
+
+    # ==== Create pcent_mortality transformed variable ====
+    # Empirical logit and IHS transform on labels
+  ir_data_emp <- ir_data_raw %>%
+    mutate(
+      pct_mortality_emp = emplogit2(
+        {{ no_dead }},
+        {{ no_tested }}
+      )
+    )
+
+    theta2 <- optimise(
+      f = IHS.loglik,
+      lower = 0.001,
+      upper = 50,
+      x = ir_data_emp$pct_mortality_emp,
+      maximum = TRUE
+    )
+
+    ir_data <- ir_data_emp %>%
+      mutate(
+        pct_mortality = IHS(
+          pct_mortality_emp,
+          theta2$maximum
+        ),
+    # add an intercept
+        int = 1
+      )
+
+    ir_data
+
+}
---
+++ R/emplogit.R
@@ -0,0 +1,49 @@
+# empirical logit number
+emplogit <- function(Y, N) {
+  top <- Y * N + 0.5
+  bottom <- N * (1 - Y) + 0.5
+  return(log(top / bottom))
+}
+
+# empirical logit two numbers
+emplogit2 <- function(Y, N) {
+  top <- Y + 0.5
+  bottom <- N - Y + 0.5
+  return(log(top / bottom))
+}
+
+emplogit3 <- function(Y, N) {
+  top <- Y + 1.5
+  bottom <- N - Y + 1.5
+  return(log(top / bottom))
+}
+
+emplogit4 <- function(p) {
+  top <- p + 0.015
+  bottom <- 1 - p + 0.015
+  return(log(top / bottom))
+}
+
+invelogit <- function(L, N) {
+  top <- exp(L) * (N + 0.5) - 0.5
+  bottom <- N * (1 + exp(L))
+  return(top / bottom)
+}
+
+# IHS transformation
+IHS <- function(x, theta) {
+  (1 / theta) * asinh(theta * x)
+}
+
+# Inverse IHS transformation
+Inv.IHS <- function(x, theta) {
+  (1 / theta) * sinh(theta * x)
+}
+
+IHS.loglik <- function(theta, x) {
+  n <- length(x)
+  xt <- IHS(x, theta)
+  log.lik <-
+    -n * log(sum((xt - mean(xt))^2)) - sum(log(1 + theta^2 * x^2))
+  return(log.lik)
+}
