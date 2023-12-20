dd9d42cd2563fdb5038313f81bfca3996c036453
njtierney <nicholas.tierney@gmail.com>
2023-12-20 11:53:31 +1000

    add some code to import and compress the CSV of the IR data

---
+++ data/import-ir-data.R
@@ -0,0 +1,26 @@
+# just read in a fraction of columns the original data, as a toy example
+# data is from https://github.com/pahanc/Mapping-insecticide-resistance
+library(tidyverse)
+library(readr)
+data_all <- as_tibble(read_rds("../demo-map-ir/data/data_all_wa.rds"))
+
+ir_data_raw <- data_all %>%
+  # create random genotypic/phenotypic variable, since I'm not sure where it is
+  mutate(
+    type = sample(
+      x = c("genotypic", "phenotypic"),
+      replace = TRUE,
+      size = n()),
+    .before = everything()
+  )
+
+write_csv(ir_data_raw, "data/ir-data-raw.csv.gz")
+
+# explore which variables might potentially be the genotypic/phenotypic indicator
+# could not find this - might need to ask Penny
+# map_dfr(data_all, n_distinct) %>%
+# pivot_longer(cols = everything(),
+#              names_to = "vars",
+#              values_to = "distinct") %>%
+#   filter(distinct <= 3) %>%
+#   arrange(distinct)
