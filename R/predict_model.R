89746dcd5379d4a3dc5a83063cf21c84523968f4
njtierney <nicholas.tierney@gmail.com>
2023-12-21 14:48:08 +1000

    helper function to extract predictions and engine name into predictions

--- R/predict_model.R
+++ R/predict_model.R
@@ -11,6 +11,24 @@
 predict_model <- function(data,
                           model) {
 
-  NULL
+  predictions <- map2(
+    .x = model,
+    .y = data,
+    .f = function(.x, .y){
+      predict(.x, as.data.frame(.y))
+    }
+  )
+
+  engine_name <- extract_engine_name(model[[1]])
+
+  new_name <- glue(".pred_{engine_name}")
+
+  bind_rows(
+    predictions,
+    .id = "fold"
+  ) %>%
+    rename(
+      !!new_name := .pred
+    )
 
 }
