bc3fcc87da652bbaf78da74f704addfec959bd11
njtierney <nicholas.tierney@gmail.com>
2023-12-21 14:48:27 +1000

    make inla model currently fit a simple lm model

--- R/build_inla_data.R
+++ R/build_inla_data.R
@@ -8,9 +8,26 @@
 #' @return
 #' @author njtierney
 #' @export
-build_inla_data <- function(ir_data = ir_data_n_star, covariate_data =
-                            out_of_sample_covariates) {
+build_inla_data <- function(ir_data,
+                            covariate_data) {
 
-  NULL
+  model_spec <-  linear_reg() %>%
+    set_mode("regression") %>%
+    set_engine("lm")
 
+  covariate_names <- covariate_data %>%
+    select(-starts_with("fold")) %>%
+    names()
+
+  workflow <- build_workflow(model_spec,
+                             outcomes = "pct_mortality",
+                             predictors = covariate_names)
+
+  data <- bind_cols(select(ir_data, pct_mortality),
+                    covariate_data)
+
+  return(
+    list(workflow = workflow,
+         data = data)
+  )
 }
--- R/gp_inla.R
+++ R/gp_inla.R
@@ -8,8 +8,11 @@
 #' @return
 #' @author njtierney
 #' @export
-gp_inla <- function(data = gp_inla_data_n_star_oos, setup = l_one_model_setup) {
+gp_inla <- function(data,
+                    setup) {
 
-  NULL
+  fit <- fit(data$workflow, as.data.frame(data$data))
+
+  fit
 
 }
