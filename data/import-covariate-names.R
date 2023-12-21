718265b30d13263c9814664bcd4f23b5be7ed790
njtierney <nicholas.tierney@gmail.com>
2023-12-21 12:09:14 +1000

    add code for importing covariate_names

---
+++ data/import-covariate-names.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(readr)
+
+covariate_names <- read_rds("../demo-map-ir/data/covariate_names_all_wa.rds")
+
+write_rds(covariate_names,
+          here::here("data/ir-data-covariates.rds"),
+          compress = "xz")
