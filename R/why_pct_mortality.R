3c7138e653475a7dad6dd16f4436a35f93f654d1
njtierney <nicholas.tierney@gmail.com>
2024-02-19 18:56:53 +1100

    explore why there are missing values for no_tested and no_dead but present for percent_mortality

---
+++ R/why_pct_mortality.R
@@ -0,0 +1,24 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param ir_data_moyes_prepared
+#' @return
+#' @author njtierney
+#' @export
+why_pct_mortality <- function(ir_data_moyes_raw) {
+
+  ir_data_moyes_raw %>%
+    select(
+      no_mosquitoes_tested,
+      no_mosquitoes_dead,
+      percent_mortality
+      ) %>%
+    filter(
+      no_mosquitoes_tested == "NR" & no_mosquitoes_dead == "NR"
+      # is.na(no_mosquitoes_tested) & is.na(no_mosquitoes_dead)
+      # !is.na(percent_mortality)
+    )
+
+}
