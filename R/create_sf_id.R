0f9caa3690216edd3c30cf06307ed13394b80696
njtierney <nicholas.tierney@gmail.com>
2024-02-19 18:55:38 +1100

    create a function to store the SF geometry as its own small dataset with a unique id (UID) to join back to the data later

---
+++ R/create_sf_id.R
@@ -0,0 +1,25 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param ir_data_moyes
+#' @return
+#' @author njtierney
+#' @export
+create_sf_id <- function(ir_data_moyes) {
+  sf_moyes <- st_as_sf(
+    ir_data_moyes,
+    coords = c("longitude", "latitude"),
+    # equivalent to "EPSG:4326" which technically is
+    # strictly lat,lon for contexts where that matters
+    crs = "OGC:CRS84"
+  )
+
+  # separating down to a unique ID and a geometry column so we can keep
+  # this as a primary key of the data
+  sf_moyes %>%
+    select(uid,
+           geometry)
+
+}
