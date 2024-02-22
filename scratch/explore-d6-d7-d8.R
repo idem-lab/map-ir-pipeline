1b7adabf16a276c50da676fd269d1de410fcf27e
njtierney <nicholas.tierney@gmail.com>
2024-02-22 11:03:41 +1100

    more scratch code to explore/understand the genotypic data

--- scratch/explore-d6-d7-d8.R
+++ scratch/explore-d6-d7-d8.R
@@ -16,5 +16,55 @@ d8 <- read_csv(
   name_repair = make_clean_names
   )
 
-head(names(d6), 30)
-head(names(d8), 30)
+View(d6)
+View(d7)
+View(d8)
+
+dim(d6)
+names(d6)
+names(ir_data)
+
+d6 %>%
+  rowid_to_column("uid") %>%
+  select(kdr_test_id,
+         # no country column - this can be inferred however
+         start_month,
+         start_year,
+         end_month,
+         end_year,
+         publication_year,
+         site_name,
+         latitude,
+         longitude,
+         # no species name!
+         no_mosquitoes_tested,
+         # no mosquitoes dead?
+         # no percent mortality?
+         # need to add type
+         # no identification method1?
+         # no identification method2?
+         generation,
+         # no information on insecticide
+         )
+
+
+
+coded_places <- tibble(
+  latitude = c(38.895865, 43.6534817),
+  longitude = c(-77.0307713, -79.3839347)
+) %>%
+  reverse_geocode(
+    lat = latitude,
+    long = longitude,
+    method = "osm",
+    full_results = TRUE
+  )
+
+coded_places$country_code
+coded_places$country
+
+
+dim(d8)
+
+names(d6)
+names(d8)
