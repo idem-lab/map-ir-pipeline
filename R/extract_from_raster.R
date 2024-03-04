fc48742f1d4ab05706dc5fb25a68226433a1e80c
njtierney <nicholas.tierney@gmail.com>
2024-03-04 16:08:16 +1100

    prefer terra::extract, code style tidy

--- R/extract_from_raster.R
+++ R/extract_from_raster.R
@@ -9,21 +9,23 @@
 #' @author njtierney
 #' @export
 extract_from_raster <- function(raster, ir_data_subset, ir_data_sf_key) {
-
   # filter down to the right SF data
   sf_subset <- semi_join(ir_data_sf_key,
-                         ir_data_subset,
-                         by = "uid")
+    ir_data_subset,
+    by = "uid"
+  )
 
-  extracted_raster <- terra::extract(raster,
-                                     sf_subset,
-                                     ID = FALSE,
-                                     method = "bilinear") %>%
+  extracted_raster <- extract(
+    raster,
+    sf_subset,
+    ID = FALSE,
+    method = "bilinear"
+  ) %>%
     as_tibble(.name_repair = make_clean_names) %>%
     mutate(
       uid = ir_data_subset$uid,
       .before = everything()
-      )
+    )
 
   ## add country information back onto the data
 
@@ -33,11 +35,10 @@ extract_from_raster <- function(raster, ir_data_subset, ir_data_sf_key) {
     select(
       uid,
       country
-      ) %>%
+    ) %>%
     left_join(
       extracted_raster,
       by = "uid"
     ) %>%
     as_tibble()
-
 }
