cb5018223c5b118e2212d8eeae6539b7a62f995c
njtierney <nicholas.tierney@gmail.com>
2024-04-09 16:45:38 +1000

    hack to provide random values of mosquitoes dead and tested

--- R/prepare_rasters_for_inner_loop.R
+++ R/prepare_rasters_for_inner_loop.R
@@ -16,20 +16,35 @@ prepare_rasters_for_inner_loop <- function(rasters = covariate_rasters,
 
   rasters_as_data <- raster_to_df(rasters)
 
+  cli_warn(
+    message = c(
+      "OK so interpret with caution here, Golding",
+      "The {.var no_mosquitoes_tested} and {.var no_mosquitoes_dead} values \\
+      are totally made up because I haven't worked out how to find the \\
+      nearest corresponding values, but I should have that sorted after \\
+      Mike tomorrow"
+    )
+  )
   rasters_w_basic_info <- rasters_as_data %>%
     mutate(
       start_year = chosen_year,
       start_month = 1,
       end_month = 12,
       end_year = chosen_year,
+      ## TODO
+      # We need to add the following variables -
+      # no_mosquitoes_tested...
+      # no_mosquitoes_dead...
+      # by interpolating them to be nearest to their corresponding
+      # values in the data frame.
+      # which can actually be done for all of the above values
+      # for the time being thi will be random values...
+      no_mosquitoes_tested = rpois(n = n(), lambda = 63),
+      no_mosquitoes_dead = rpois(n = n(), lambda = 48),
       int = 1,
       .before = everything()
     )
 
-
-  # need to add the following variables?
-  # no_mosquitoes_tested...
-  # no_mosquitoes_dead...
 
   # names(rasters_w_basic_info) <- glue("raster_{seq_len(length(rasters))}")
 
